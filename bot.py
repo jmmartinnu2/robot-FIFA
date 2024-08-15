e9fa55371c5ee8eb23e6314295ca65ae3dcbf0b2
Jm <93231115+jmmartinnu2@users.noreply.github.com>
2024-08-15 16:02:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,13 @@ import discord
 import json
 import os
 from dotenv import load_dotenv
-import streamlit as st
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-# Configurar Streamlit
-st.title("Bot FIFA")
-st.write("Interfaz para controlar y monitorear el bot de Discord.")
-
 # Cargar el archivo JSON con las licencias usando una ruta relativa
 with open(os.path.join(os.path.dirname(__file__), 'config.json'), encoding='utf-8') as f:
     agentes = json.load(f)["content"]
-
-# Mostrar información básica o logs en Streamlit
-st.write("Número de agentes cargados:", len(agentes))
 
 # Configurar los intents necesarios
 intents = discord.Intents.default()
@@ -27,7 +19,6 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    st.write(f'Bot {client.user} conectado')
     print(f'Bot {client.user} conectado')
 
 @client.event
@@ -52,7 +43,7 @@ async def on_message(message):
         elif tipo.lower() == "id":
             resultados = [agente for agente in agentes if agente["fifaId"].lower() == valor]
         elif tipo.lower() == "nombre":
-            resultados = [agente for agente in agentes if valor in (agente["firstName"].lower() + " " + agente["lastName"].lower())]
+            resultados = [agente for agente in agentes si valor in (agente["firstName"].lower() + " " + agente["lastName"].lower())]
 
         if resultados:
             respuesta = "\n\n".join([f"Nombre: {agente['firstName']} {agente['lastName']}\nLicencia: {agente['licenseNumber']}\nFIFA ID: {agente['fifaId']}\nEstado: {agente['licenseStatus']}\nAutorizado para menores: {agente['authorisedMinors']}" for agente in resultados])
@@ -61,11 +52,10 @@ async def on_message(message):
 
         await message.channel.send(respuesta)
 
-# Obtener el token de una variable de entorno
+# Obtener el token de Discord de una variable de entorno
 token = os.getenv('DISCORD_TOKEN')
 
 if token is None:
-    st.error("El token de Discord no está configurado en las variables de entorno.")
     raise ValueError("El token de Discord no está configurado en las variables de entorno.")
 
 # Ejecutar el bot de Discord
